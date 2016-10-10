3567ec9106c1034c3a99b053b92eed50861c60c8
Aji Lantang Mardika & Alexander Hendrawan tjahja <pair+ajilantang.alexanderHT@devbootcamp.com>
2016-10-10 20:11:35 +0700

    address_book

--- address_book.js
+++ address_book.js
@@ -1,1 +1,123 @@
+//write your code here
+"use strict"
 
+const repl = require('repl'); // optional
+const sqlite = require('sqlite3').verbose();
+
+// write your code here
+var file = 'addressbook.db';
+var db = new sqlite.Database(file);
+
+class Kontak {
+  constructor(person) {
+    this.id = null
+    this.firstname  = person['firstname']
+    this.lastname = person['lastname']
+    this.email = person["email"]
+    this.phone = person["phone"]
+    this.birthdate = person["birthdate"]
+  }
+
+  // setter
+  // set id(){
+  //   // this
+  // }
+
+  set setFirstname(nama){
+    db.serialize(function(){
+
+      db.run(`UPDATE kontak SET firstname = '${nama}' WHERE id = '${this.id}'`, function(err){
+        if(err){
+          console.log(err);
+        }else{
+         this.firstname = nama
+        }
+      });
+
+    });
+  }
+
+  set setEmail(email){
+    db.serialize(function(){
+
+      db.run(`UPDATE kontak SET email = '${email}' WHERE id = '${this.id}'`, function(err){
+        if(err){
+          console.log(err);
+        }else{
+          this.email = email
+        }
+      });
+
+    });
+  }
+
+  set setPhone(phone){
+    db.serialize(function(){
+
+      db.run(`UPDATE kontak SET phone = '${phone}' WHERE id = '${this.id}'`, function(err){
+        if(err){
+          console.log(err);
+        }else{
+          this.phone = phone;
+        }
+      });
+
+    });
+  }
+
+  // getter
+  // get id(){
+  //   return this.id;
+  // }
+
+  get getFirstname(){
+    return this.firstname;
+  }
+
+  // method
+  save(){
+    // db.serialize(function(){
+      db.run(`INSERT INTO kontak (firstname,lastname,email, phone,birthdate) VALUES ('${this.firstname}', '${this.lastname}', '${this.email}','${this.phone}','${this.birthdate}');`, function(err){
+        if(err){
+          console.log(err);
+        }else{
+          // select
+          db.each(`SELECT id FROM kontak where phone = '${this.phone}' LIMIT 1;`, function(err, row) {
+             this.id = row.id;
+             console.log(this.id);
+          });
+
+        }
+      });
+    // });
+  // end addKontak
+  }
+}
+
+var person = {
+  firstname : "dharmadi",
+  lastname : "tanamas",
+  email : "dharmaditanamas",
+  phone : "123123190",
+  birthdate : "1998-05-01"
+}
+
+var person1 = new Kontak(person)
+console.log(person1.id); // null
+person1.save()
+console.log(person1.id); // 1
+person1.firstname = "hallo world"
+console.log(person1.firstname);
+// console.log("asdasdasd");
+
+// class Grup {
+//   constructor() {
+//
+//   }
+// }
+//
+// class Kontak_Grup {
+//   constructor() {
+//
+//   }
+// }
---
+++ setup.js
@@ -0,0 +1,128 @@
+//write your code here
+"use strict"
+
+const repl = require('repl'); // optional
+const sqlite = require('sqlite3').verbose();
+
+// write your code here
+var file = 'addressbook.db';
+var db = new sqlite.Database(file);
+
+//SQL statements
+var CREATE_TABLE_GROUP = "CREATE TABLE IF NOT EXISTS 'group' (id INTEGER PRIMARY KEY AUTOINCREMENT, groupname TEXT NOT NULL, create_at DATE)"
+
+var CREATE_TABLE_KONTAK = "CREATE TABLE IF NOT EXISTS kontak (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT,email TEXT, phone TEXT,birthdate DATE);";
+
+var CREATE_TABLE_KONTAK_GROUP ="CREATE TABLE IF NOT EXISTS kontak_group (id INTEGER PRIMARY KEY AUTOINCREMENT, kontak_id INTEGER, group_id INTEGER, FOREIGN KEY(group_id) REFERENCES 'group'(id), FOREIGN KEY(kontak_id) REFERENCES kontak(id));"
+
+var SEED_DATA_GROUP = "INSERT INTO 'group' (groupname,create_at) VALUES ('blandford', '1992-12-02');";
+
+var SEED_DATA_KONTAK = "INSERT INTO kontak (firstname,lastname,email, phone,birthdate) VALUES ('alexander', 'HT', 'alexander@hackti8.com','14045-911','1992-12-02');"
+
+var SEED_DATA_KONTAK_GROUP = "INSERT INTO kontak_group ( id,group_id, kontak_id) VALUES (1, 1 , 1);"
+
+// create table
+let createTableGroup = () => {
+  db.serialize(function(){
+
+    db.run(CREATE_TABLE_GROUP, function(err){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Succes! create table group");
+      }
+    });
+
+  });
+}
+
+ let createTableKontak = () => {
+  db.serialize(function(){
+
+    db.run(CREATE_TABLE_KONTAK, function(err){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Succes! create table kontak");
+      }
+    });
+
+  });
+}
+
+let createTableKontakGroup = () => {
+  db.serialize(function(){
+
+    db.run(CREATE_TABLE_KONTAK_GROUP, function(err){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Succes! create table kontak group");
+      }
+    });
+
+  });
+}
+
+// loop Database
+// var dataGroup = [{"groupname": "lol1", "create_at": "DATE()"}, {"groupname": "lil2", "create_at": "DATE()"}]
+// function inputKontak(){
+//   for(let i = 0; i < dataGroup.length; i++){
+//     var SEED_DATA_GROUP = `INSERT INTO 'group' (groupname,create_at) VALUES ('${dataGroup[i].groupname}', '${dataGroup[i].create_at}');`;
+//   }
+//
+
+//console.log(SEED_DATA_GROUP);
+
+// seed data
+let seedDatagroup = () => {
+  db.serialize(function(){
+
+    db.run(SEED_DATA_GROUP, function(err){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Succes! insert grup");
+      }
+    });
+
+  });
+}
+
+
+let seedDataKontak = () => {
+  db.serialize(function(){
+
+    db.run(SEED_DATA_KONTAK, function(err){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Succes! insert 1 kontak");
+      }
+    });
+
+  });
+}
+
+let seedDataKontakGroup = () => {
+  db.serialize(function(){
+
+    db.run(SEED_DATA_KONTAK_GROUP, function(err){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Succes! insert 1 kontak grup");
+      }
+    });
+
+  });
+}
+
+//
+createTableGroup()
+createTableKontak()
+createTableKontakGroup()
+
+seedDatagroup()
+seedDataKontak()
+seedDataKontakGroup()
