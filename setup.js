f5ce798af9223aac0f56bfe30040f15ef7539532
Izhharuddin Syanmil & Septian A Tama <pair+syanmil.tamatamvan@devbootcamp.com>
2016-10-10 14:57:52 +0700

    seeding data

--- setup.js
+++ setup.js
@@ -9,7 +9,9 @@ let db = new sqlite.Database(file); // SQL Statement
 let CREATE_TABLE_CONTACTS = "CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, nama TEXT, perusahaan TEXT, no_telp TEXT, email TEXT)" ;
 let CREATE_TABLE_GROUPS = "CREATE TABLE IF NOT EXISTS groups (id INTEGER PRIMARY KEY AUTOINCREMENT, nama TEXT)";
 let CREATE_TABLE_GC = "CREATE TABLE IF NOT EXISTS group_contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, group_id INTEGER, contact_id INTEGER, FOREIGN KEY(group_id) REFERENCES groups(id), FOREIGN KEY(contact_id) REFERENCES contacts(id))";
-
+let SEED_CONTACTS = "INSERT INTO contacts VALUES (null, 'susan', 'MNC', '0211234567', 'google@yahoo.com' )"
+let SEED_GROUPS = "INSERT INTO groups VALUES (null, 'nama group doank')"
+let SEED_GC = "INSERT INTO group_contacts VALUES (null, 1, 1)"
 // CREATE_TABLE
 let createTable = () => {
   // Run SQL one at a time
@@ -20,7 +22,7 @@ let createTable = () => {
         console.log(err);
       } else {
         console.log('TABLE contacts CREATED');
-        // seedData();
+        seedContacts();
       }
     });
     db.run(CREATE_TABLE_GROUPS, (err) => {
@@ -28,7 +30,7 @@ let createTable = () => {
         console.log(err);
       } else {
         console.log('TABLE groups CREATED');
-        // seedData();
+        seedGroups();
       }
     });
     db.run(CREATE_TABLE_GC, (err) => {
@@ -36,21 +38,41 @@ let createTable = () => {
         console.log(err);
       } else {
         console.log('TABLE group_contacts CREATED');
-        // seedData();
+        seedGC();
       }
     });
   });
 }
 // SEED_DATA
-// let seedData = () => {
-//   // Your code here
-//   db.run(SEED_DATA, (err) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log('DATA INSERTED');
-//     }
-//   })
-// }
+let seedContacts = () => {
+  // Your code here
+  db.run(SEED_CONTACTS, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('DATA INSERTED');
+    }
+  })
+}
+let seedGroups = () => {
+  // Your code here
+  db.run(SEED_GROUPS, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('DATA INSERTED');
+    }
+  })
+}
+let seedGC = () => {
+  // Your code here
+  db.run(SEED_GC, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('DATA INSERTED');
+    }
+  })
+}
 
 createTable();
