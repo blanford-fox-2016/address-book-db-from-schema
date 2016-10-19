5a4d9d5cad6843cc70980f8185f6c43527772fbd
Aji Lantang Mardika & Alexander Hendrawan tjahja <pair+ajilantang.alexanderHT@devbootcamp.com>
2016-10-19 15:55:42 +0700

    update

--- address_book.js
+++ address_book.js
@@ -18,14 +18,8 @@ class Kontak {
     this.birthdate = person["birthdate"]
   }
 
-  // setter
-  // set id(){
-  //   // this
-  // }
-
   set setFirstname(nama){
     db.serialize(function(){
-
       db.run(`UPDATE kontak SET firstname = '${nama}' WHERE id = '${this.id}'`, function(err){
         if(err){
           console.log(err);
@@ -33,13 +27,11 @@ class Kontak {
          this.firstname = nama
         }
       });
-
     });
   }
 
   set setEmail(email){
     db.serialize(function(){
-
       db.run(`UPDATE kontak SET email = '${email}' WHERE id = '${this.id}'`, function(err){
         if(err){
           console.log(err);
@@ -47,13 +39,11 @@ class Kontak {
           this.email = email
         }
       });
-
     });
   }
 
   set setPhone(phone){
     db.serialize(function(){
-
       db.run(`UPDATE kontak SET phone = '${phone}' WHERE id = '${this.id}'`, function(err){
         if(err){
           console.log(err);
@@ -61,22 +51,16 @@ class Kontak {
           this.phone = phone;
         }
       });
-
     });
   }
-
-  // getter
-  // get id(){
-  //   return this.id;
-  // }
 
   get getFirstname(){
     return this.firstname;
   }
 
-  // method
+  // method save
   save(){
-    // db.serialize(function(){
+
       db.run(`INSERT INTO kontak (firstname,lastname,email, phone,birthdate) VALUES ('${this.firstname}', '${this.lastname}', '${this.email}','${this.phone}','${this.birthdate}');`, function(err){
         if(err){
           console.log(err);
@@ -89,35 +73,109 @@ class Kontak {
 
         }
       });
-    // });
   // end addKontak
+  }
+
+  deleting(idkontak){
+    db.run(`PRAGMA foreign_keys = ON;`,
+      function (err){
+        if (err) {
+          console.log(err);
+        }else {
+          db.run(` DELETE FROM 'kontak' where id='${idkontak}'`,function (error){
+            if (err) {
+              console.log(`id kontak ${idkontak} tidak teradaftar`);
+            }else {
+
+            }
+          });
+        }
+      });
   }
 }
 
+class Group{
+
+  addGrup(groupname){
+    db.run(`INSERT INTO 'group' (groupname, create_at) VALUES ('${groupname}', DATE());`, function(err){
+      if(err){
+        console.log(err);
+      }
+    });
+  }// end add
+  deleting(idgroup){
+    db.run(`PRAGMA foreign_keys = ON;`,
+      function (err){
+        if (err) {
+          console.log(err);
+        }else {
+          db.run(` DELETE FROM 'group' where id='${idgroup}';`,function (error){
+            if (err) {
+              console.log(`id group ${idgroup} tidak teradaftar`);
+            }else {
+
+            }
+          });
+        }
+      });
+  }
+} // end Group
+
+class Kontak_Group {
+
+  addKontakGroup(kontakid, groupid){
+    db.run(`INSERT INTO kontak_group (kontak_id,group_id) VALUES ('${kontakid}, ${groupid};`, function(err){
+      if (err) {
+        console.log(err);
+      }
+    })
+  }
+
+  delete_kg_kotak(kotakid,groupid){
+    db.run(`PRAGMA foreign_keys = ON;`,
+      function (err){
+        if (err) {
+          console.log(err);
+        }else {
+          db.run(` DELETE FROM 'kontak_group' where kontak_id='${kotakid} AND group_id= '${groupid}';`,function (error){
+            if (err) {
+              console.log(`id kontak ${kotakid} dalam group tidak teradaftar`);
+            }else {
+
+            }
+          });
+        }
+      });
+  }
+} // end class Kontak_Group
+
+// data objek yang nanti di masukan kedalam parameter class Kontaks
 var person = {
-  firstname : "dharmadi",
-  lastname : "tanamas",
-  email : "dharmaditanamas",
-  phone : "123123190",
+  firstname : "ken",
+  lastname : "chen",
+  email : "ken chen",
+  phone : "0838067811",
   birthdate : "1998-05-01"
 }
 
-var person1 = new Kontak(person)
-console.log(person1.id); // null
-person1.save()
-console.log(person1.id); // 1
-person1.firstname = "hallo world"
-console.log(person1.firstname);
-// console.log("asdasdasd");
+// ka kalo mau masukin yang lain harus di comment, kalo mau delete juga sama yang add di comment dulu
 
-// class Grup {
-//   constructor() {
-//
-//   }
-// }
-//
-// class Kontak_Grup {
-//   constructor() {
-//
-//   }
-// }
+// objek kontak
+// data person ada di atas
+var person1 = new Kontak(person)
+console.log(person1.id); // output null
+person1.save()
+console.log(person1.id); // output 1
+// person1.deleting(1);
+
+// ganti nama yang ada di objek bukan di database, kalo mau yang di database pake yang person1.setFirstname = "nama depan"
+// person1.firstname = "hallo world"
+// console.log(person1.firstname); // cetak nama depan dari objek yang di buat
+
+// objek group
+// var group1 = new Group()
+// group1.addGrup("dunia anak") // addGrup([nama grup yang akan dimasukan])
+
+// objek Kontak_Group
+// var kontak_group1 = new Kontak_Group()
+// kontak_group1.addKontakGroup() // addKontakGroup([id kontak],[id group])
