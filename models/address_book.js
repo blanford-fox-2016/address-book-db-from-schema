36d3621a01d5992ffc77f6d52172189c1d28dd7f
Aji Lantang Mardika & Septian A Tama <pair+ajilantang.tamatamvan@devbootcamp.com>
2016-10-19 18:44:24 +0700

    reorganizing the module

--- models/address_book.js
+++ models/address_book.js
@@ -25,24 +25,44 @@ export class Adress_Book {
   }
 
   static add_group(group) {
-    db.run (`INSERT INTO group (name) VALUES ('${group.group_name}');`, (error) => {
-      (error) ? console.error(error) : console.log("ini masuk");;
+    db.run (`INSERT INTO \`group\` (group_name) VALUES ('${group.group_name}');`, (error) => {
+      (error) ? console.error(error) : console.log("group is updated");;
     });
   }
 
-  static update_user(user) {
-
+  static update_user(user, id) {
+    db.serialize(function(){
+     db.run(`UPDATE user SET first_name = '${user.first_name}', last_name ='${user.last_name}', user_email ='${user.user_email}', '${user.user_address}' WHERE id = '${id}'`, function(err){
+       if(err){
+         console.log(err);
+       }else{
+        console.log("user is updated");
+       }
+     });
+   });
   }
 
-  static update_group(group) {
-
+  static update_group(group, id) {
+    db.serialize(function(){
+     db.run(`UPDATE group SET first_name = '${group.group_name}' WHERE id = '${id}'`, function(err){
+       if(err){
+         console.log(err);
+       }else{
+        console.log("group is updated");
+       }
+     });
+   });
   }
 
-  static delete_user(user) {
-
+  static delete_user(id) {
+    db.run(` DELETE FROM 'user' where id='${id}'`, (err) => {
+      err ? console.error(err) : console.log("deleted");
+    }
   }
 
-  static delete_group(group) {
-
+  static delete_group(id) {
+    db.run(` DELETE FROM 'group' where id='${id}'`, (err) => {
+      err ? console.error(err) : console.log("deleted");
+    }
   }
 }
--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import {Group} from './groups.js';
+
 export class User {
   constructor(component) {
     this.user_id = component['user_id'];
@@ -7,13 +9,6 @@ export class User {
     this.last_name = component['last_name'];
     this.user_email = component['user_email'];
     this.user_address = component['user_address'];
+    this.group = component['group'] || null;
   }
-
-
 }
-
-let GROUPS = "CREATE TABLE IF NOT EXISTS `group` (group_id INTEGER PRIMARY KEY, user TEXT , group_name TEXT NOT NULL);";
-
-let ADDRESS_BOOKS = "CREATE TABLE IF NOT EXISTS `contact` (contact_id INTEGER PRIMARY KEY AUTOINCREMENT, trackusers INTEGER, trackgroup INTEGER,FOREIGN KEY(trackusers) REFERENCES user(user_id), FOREIGN KEY(trackgroup) REFERENCES `group`(group_id));";
-
-let USERS = "CREATE TABLE IF NOT EXISTS `user` (user_id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT, last_name TEXT, user_email TEXT, user_address TEXT, trackgroup INTEGER, FOREIGN KEY(trackgroup) REFERENCES group(group_id));";
